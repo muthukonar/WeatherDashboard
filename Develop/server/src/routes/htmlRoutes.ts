403b20608e0c6c48d4a20740a1eeabc18badec32
pechikonar <muthukonaruot@gmail.com>
2025-01-29 19:56:14 -0500

    Fixed htmlRoutes.ts issues

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -1,15 +1,14 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { Router } from 'express';
-import express from 'express';
+import { Router, type Request, type Response } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.use(express.static(path.join(__dirname, '../../../client')));
-router.get('/', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+
+router.get('/', (_req:Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
   });
 
 
